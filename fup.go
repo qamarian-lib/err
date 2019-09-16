afdace4a4de64d0afa598c30f099fed9aa24c3c8
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-09-16 20:32:33 +0100

    Progress 2

--- fup.go
+++ fup.go
@@ -5,16 +5,21 @@ import (
 	"fmt"
 )
 
+// Fup () fully unwraps an error, then parses the result into a string. Take for example,
+// err A (Foo) wraps err B (Bar), and err B (Bar) wraps err C (Tar). If we supply err A as
+// an argument to this function, it would return a string like this: "Foo [Bar [Tar]]"
 func Fup (err error) (string) {
 	return fullUnwrapParse (err)
 }
 
-func fullUnwrapParse (err error) (string) {
-	secondary := errors.Unwrap (err)
-	if secondary == nil {
-		return err.Error ()
-	} else {
-		sec := fullUnwrapParse (secondary)
-		return fmt.Sprintf ("%s [%s]", err.Error (), sec)
+	// fullUnwrapParse () assists Fup (), it was not designed to stand alone. That is
+	// the reason it was indented.
+	func fullUnwrapParse (err error) (string) {
+		secondary := errors.Unwrap (err)
+		if secondary == nil {
+			return err.Error ()
+		} else {
+			sec := fullUnwrapParse (secondary)
+			return fmt.Sprintf ("%s [%s]", err.Error (), sec)
+		}
 	}
-}
