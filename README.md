6988af3f52e412099e7787f6c2ebea9fedfe4edb
Ibrahim Oladipupo Qamardeen <kensharval@gmail.com>
2019-10-06 02:38:44 +0100

    Progress 4

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # err
-This package contains functions that could manipulate data type 
+
+Package err contains functions that could manipulate the data type defined in package
 [github.com/qamarian-dtp/err](https://github.com/qamarian-dtp/err). See
 [GoDoc](https://godoc.org/github.com/qamarian-lib/err) for more info.
--- doc.go
+++ doc.go
@@ -1,5 +1,8 @@
-// This package contains functions that could manipulate data type
+// Package err contains functions that could manipulate the data type defined in package
 // 'github.com/qamarian-dtp/err'.
-// Always use a version of this package corresponding with the version of the data type.
-// For example, use only v1.2.2 with v1.2.2. Never use v1.2.0 with v1.2.3, etc.
+//
+// Always use a version of this package corresponding with the version of its data type.
+// For example, use only v1.2.2 with v1.2.2. Never use v1.2.0 of
+// github.com/qamarian-lib/err with v1.2.3 of github.com/qamarian-dtp/err, etc.
+//
 package err
